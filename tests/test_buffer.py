72e1f7a8d3ba4a4024432f0e7de9af0e8fa912f5
rdeioris <roberto.deioris@gmail.com>
2022-02-21 17:51:06 +0100

    added tests

---
+++ tests/test_buffer.py
@@ -0,0 +1,17 @@
+import unittest
+from compushady import Buffer, HEAP_UPLOAD, HEAP_READBACK
+
+
+class BufferTests(unittest.TestCase):
+
+    def test_simple_upload(self):
+        b0 = Buffer(8, HEAP_UPLOAD)
+        b0.upload(b'hello!!!')
+        self.assertEqual(b0.readback(), b'hello!!!')
+
+    def test_simple_upload_and_copy(self):
+        b0 = Buffer(8, HEAP_UPLOAD)
+        b1 = Buffer(8, HEAP_READBACK)
+        b0.upload(b'hello!!!')
+        b0.copy_to(b1)
+        self.assertEqual(b0.readback(), b1.readback())
---
+++ tests/test_texture2d.py
@@ -0,0 +1,29 @@
+import unittest
+from compushady import Texture2D, Buffer, HEAP_UPLOAD, HEAP_READBACK
+from compushady.formats import R8G8B8A8_UINT, get_pixel_size
+import compushady.config
+compushady.config.set_debug(True)
+
+
+class Texture2DTests(unittest.TestCase):
+
+    def test_simple_upload(self):
+        t0 = Texture2D(2, 2, R8G8B8A8_UINT)
+        b0 = Buffer(t0.size, HEAP_UPLOAD)
+        b1 = Buffer(t0.size)
+        for y in range(0, t0.height):
+            b0.upload(b'\xDE\xAD\xBE\xEF' * 2, offset=t0.row_pitch * y)
+        b0.copy_to(t0)
+        t0.copy_to(b1)
+        b1.copy_to(b0)
+        self.assertEqual(b0.readback(4), b'\xDE\xAD\xBE\xEF')
+
+    def test_simple_upload2d(self):
+        t0 = Texture2D(2, 2, R8G8B8A8_UINT)
+        b0 = Buffer(t0.size, HEAP_UPLOAD)
+        b1 = Buffer(t0.size)
+        b0.upload2d(b'\xDE\xAD\xBE\xEF', t0.row_pitch, t0.width, t0.height, get_pixel_size(R8G8B8A8_UINT))
+        b0.copy_to(t0)
+        t0.copy_to(b1)
+        b1.copy_to(b0)
+        self.assertEqual(b0.readback(4), b'\xDE\xAD\xBE\xEF')
