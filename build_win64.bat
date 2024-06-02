117da8c13b8ae1996ada0ca647c9b0588100970a
rdeioris <roberto.deioris@gmail.com>
2024-06-02 09:33:30 +0200

    added build_win64.bat

---
+++ build_win64.bat
@@ -0,0 +1,7 @@
+%1/Python36/python.exe setup.py bdist_wheel
+%1/Python37/python.exe setup.py bdist_wheel
+%1/Python38/python.exe setup.py bdist_wheel
+%1/Python39/python.exe setup.py bdist_wheel
+%1/Python310/python.exe setup.py bdist_wheel
+%1/Python311/python.exe setup.py bdist_wheel
+%1/Python312/python.exe setup.py bdist_wheel
